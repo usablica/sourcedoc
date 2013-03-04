fafbc14ef578eede8c8842116dd71d2e84ed7218
Afshin Mehrabani <afshin.meh@gmail.com>
2013-03-05 00:04:38 +0330

    Problem with .gitignore

--- config.js
+++ config.js
@@ -8,6 +8,10 @@ exports.systemConfigs = {
   "sourceDocInitFile": ".sourcedoc.yml",
   //MongoDB connection config
   "mongoDbConnection": "mongodb://localhost/sourcedoc",
+  //valid hook ip(s), SourceDoc accept hook requests only from these ip(s)
+  "validHookIp": [
+    
+  ],
   //{0} for username, {1} for project name and {2} for revision number (e.g. docs/afshinm/persianjs/2)
   "docsOutput": "docs/{0}/{1}/{2}",
   "engines": {
